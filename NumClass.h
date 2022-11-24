5655efd10f693f3d6b6640673449a9f43c621248
Jameely <119011959+Jameely@users.noreply.github.com>
2022-11-25 00:01:43 +0200

    Add files via upload

---
+++ NumClass.h
@@ -0,0 +1,10 @@
+int isArmstrong(int x);
+int isPalindrome(int x);
+int isPrime(int x);
+int isStrong(int x);
+int isPalindromeRec(int x);
+int HelpisPalindrome(int x , int ReverseNum );
+int isArmstrongRec(int x);
+int Help1isArmstrong(int x, int);
+int Help2isArmstrong(int x, int );
+int FindTheDigitsNumRec(int x);
